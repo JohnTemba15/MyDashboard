64fd1d3d350dbaaef58fc645446bea72ce74d420
jotemjhb025 <jotemjhb025@student.wethinkcode.co.za>
2026-09-24 19:16:00 +0200

    feat: Create database file to get environment and create_client

---
+++ database.py
@@ -0,0 +1,13 @@
+from supabase import create_client, Client
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+url = os.getenv("SUPABASE_URL")
+key = os.getenv("SUPABASE_KEY")
+
+if not url or not key:
+    raise ValueError("SUPABASE_URL and SUPABASE_KEY must be set in your .env file")
+
+supabase: Client = create_client(url, key)
